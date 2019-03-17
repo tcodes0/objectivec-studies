273137bce1b37e8502431aa5ce0022da219a3ac5
Raphael Thomazella <thomazella9@gmail.com>
2019-03-17 14:46:09 -0300

    Initial Commit

---
+++ studies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  studies
+//
+//  Created by Raphael on 3/17/19.
+//  Copyright © 2019 Raphael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ studies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  studies
+//
+//  Created by Raphael on 3/17/19.
+//  Copyright © 2019 Raphael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
